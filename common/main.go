8bb3db2b1fac74eb8bdf774ffc0c31f3560a0823
unnamedtiger <unnamedtiger@outlook.com>
2024-08-19 10:47:18 +0200

    Move main function into common library

---
+++ common/main.go
@@ -0,0 +1,149 @@
+package common
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"runtime/debug"
+	"strings"
+
+	sitter "github.com/smacker/go-tree-sitter"
+	"github.com/smacker/go-tree-sitter/golang"
+)
+
+func Main(plugins ...*Plugin) {
+	// handling command line flags and parameters
+	output := flag.String("o", "terminal", "output format [terminal, csv, json]")
+	version := flag.Bool("V", false, "print version and exit")
+
+	flag.Parse()
+	directories := flag.Args()
+
+	if version != nil && *version {
+		info, ok := debug.ReadBuildInfo()
+		if !ok {
+			fmt.Printf("check (unknown)\n")
+			os.Exit(0)
+		}
+		fmt.Printf("check %s %s\n", info.Main.Version, info.GoVersion)
+		os.Exit(0)
+	}
+
+	if output != nil && *output != "terminal" && *output != "csv" && *output != "json" {
+		fmt.Fprintf(os.Stderr, "invalid output format\n")
+		os.Exit(2)
+	}
+
+	// checking that all plugins are usable in this tool
+	for _, plugin := range plugins {
+		for _, ext := range plugin.Extensions {
+			if ext != "go" {
+				fmt.Fprintf(os.Stderr, "unable to use plugin %s: unknown extension %s\n", plugin.Name, ext)
+				os.Exit(2)
+			}
+		}
+	}
+
+	// looping over all directories and passing the files to the plugins
+	violations := []Violation{}
+	for _, dir := range directories {
+		err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "unable to walk directory %s: %s\n", path, err)
+				os.Exit(2)
+			}
+
+			if d.IsDir() {
+				return nil
+			}
+
+			name := d.Name()
+			ext := filepath.Ext(name)
+			ext = strings.TrimPrefix(ext, ".")
+
+			for _, plugin := range plugins {
+				if plugin.HandlesExtension(ext) {
+					vios, err := makePluginHandleFile(plugin, path, ext)
+					if err != nil {
+						fmt.Fprintf(os.Stderr, "%s\n", err)
+						os.Exit(2)
+					}
+					violations = append(violations, vios...)
+				}
+			}
+			return nil
+		})
+
+		if err != nil {
+			os.Exit(2)
+		}
+	}
+
+	// building up the report and outputting it
+	report := Report{Violations: violations}
+	if output == nil || *output == "terminal" {
+		for _, vio := range report.Violations {
+			fmt.Println(vio.StringPretty(true))
+		}
+	} else if *output == "csv" {
+		var buf bytes.Buffer
+		err := report.WriteCsv(&buf)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to write violations to csv: %s", err)
+			os.Exit(2)
+		}
+		fmt.Print(buf.String())
+	} else if *output == "json" {
+		bytes, err := json.Marshal(report)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unable to write violations to json: %s", err)
+			os.Exit(2)
+		}
+		fmt.Println(string(bytes))
+	}
+
+	// exit with correct code
+	for _, vio := range report.Violations {
+		if vio.Justification == "" {
+			os.Exit(1)
+		}
+	}
+	os.Exit(0)
+}
+
+func makePluginHandleFile(plugin *Plugin, path string, ext string) ([]Violation, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read file %s: %s\n", path, err)
+	}
+	parser := sitter.NewParser()
+	if ext == "go" {
+		parser.SetLanguage(golang.GetLanguage())
+	} else {
+		return nil, fmt.Errorf("unknown extension: %s\n", ext)
+	}
+	tree, err := parser.ParseCtx(context.Background(), nil, content)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse file %s: %s\n", path, err)
+	}
+	root := tree.RootNode()
+
+	a := &Analysis{
+		Content: content,
+		Root:    root,
+
+		PluginName: plugin.Name,
+		FilePath:   path,
+	}
+
+	err = plugin.Run(a)
+	if err != nil {
+		return nil, fmt.Errorf("[%s] unable to check file %s: %s\n", plugin.Name, path, err)
+	}
+	return a.Violations, nil
+}
--- wrapper/go.mod
+++ wrapper/go.mod
@@ -3,10 +3,11 @@ module github.com/unnamedtiger/check/wrapper
 go 1.22.4
 
 require (
-	github.com/smacker/go-tree-sitter v0.0.0-20240625050157-a31a98a7c0f6
 	github.com/unnamedtiger/check/common v0.0.0
 	github.com/unnamedtiger/check/plugins/unwanted_imports v0.0.0
 )
+
+require github.com/smacker/go-tree-sitter v0.0.0-20240625050157-a31a98a7c0f6 // indirect
 
 replace (
 	github.com/unnamedtiger/check/common => ../common
